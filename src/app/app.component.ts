dd99db7fa063b56617a62e9f72c79f7c61c04d5d
Gabriel Gonçalves Marques <Gabriel Gonalves Marques>
2017-11-08 17:52:20 -0200

    Correção de bugs
    Signed-off-by: Gabriel Gonçalves Marques <Gabriel Gonçalves Marques>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component,ViewChild,OnInit } from '@angular/core';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
+import { User } from './shared/user.model';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database';
+declare var $: any;
+
 
 @Component({
   selector: 'app-root',
@@ -14,21 +18,54 @@ export class AppComponent  implements OnInit{
   
   @ViewChild('WarningMessageComponent') child;
 
-  constructor(public afAuth: AngularFireAuth){
-    this.user = this.afAuth.authState
+  constructor(public afAuth: AngularFireAuth,public db: AngularFireDatabase){
+    this.userAuth = this.afAuth.authState
   }
-
-  user: Observable<firebase.User>
-  userObj:firebase.User = null;
+  userDatabaseObservable: FirebaseListObservable<User[]>;
+  userDatabase: User;
+  userAuth: Observable<firebase.User>
+  userAuthObj:firebase.User = null;
   userVerified = false;
   progressIconShown = true;
 
   ngOnInit() {
-    this.user.forEach(item=>{
-      this.userObj = item;
-      this.userVerified = true;
+    this.updateUserAuth();
+  }
+
+  updateUserAuth(){
+    this.userAuth.forEach(item=>{
+      this.userAuthObj = item;
+      this.progressIconShown = false;
+      this.updateUserDatabase();
     });
+  }
+  updateUserDatabase(){
+    this.userDatabaseObservable = this.db.list('users_'+this.userAuthObj.uid+"/");
     
+    this.userDatabaseObservable.forEach((item) => {
+      
+      this.userDatabase = null;
+      
+      item.forEach((item) => {
+        console.log(item);
+        this.userDatabase = item;
+      
+        if(!item){
+          console.log('lalal');
+          this.userVerified = false;
+            $('settingsModal').show();
+        }else{
+          this.userVerified = true;  
+        }
+      });
+      if(!this.userDatabase){
+        console.log('lalal');
+        this.userVerified = false;
+          $('settingsModal').show();
+      }else{
+        this.userVerified = true;  
+      }
+    });
   }
 
   showProgressIcon(){
